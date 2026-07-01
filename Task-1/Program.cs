40f9e5010e901dadee9c00833344110f3554ac64
Noora AlHusaini <hlhussaini402@gmail.com>
2026-07-01 09:31:08 +0400

    Create personal info cart

---
+++ Task-1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task-1 create personal info cart 
+            string name = "Noora saif";
+            int age = 24;
+            double height = 1.53;
+            bool isStudent = false;
+
+            Console.Write("Name: " + name + ", Age: " + age + ", Height: " + height + ", Student: " + isStudent);
+        }
+    }
+}
