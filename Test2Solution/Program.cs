0e6f8ab01c22cdf076523f4c6b6c5d2272347cc9
Noora AlHusaini <hlhussaini402@gmail.com>
2026-07-03 10:44:35 +0400

    implemented Task-1 Countdown Time

---
+++ Test2Solution/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Test2Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task-1 Countdown Time
+            Console.WriteLine("Enter a starting number");
+            int startNum = int.Parse(Console.ReadLine());
+
+            for (int i = startNum; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Liftoff!");
+
+
+        }
+    }
+}
