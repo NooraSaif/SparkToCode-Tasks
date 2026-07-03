d7aecd292eacf7d564aadfebf4ae23380f4dab0e
Noora AlHusaini <hlhussaini402@gmail.com>
2026-07-03 15:18:58 +0400

    implemented Task-2 Sum of number 1 to N

--- Test2Solution/Program.cs
+++ Test2Solution/Program.cs
@@ -5,16 +5,27 @@
         static void Main(string[] args)
         {
             //Task-1 Countdown Time
-            Console.WriteLine("Enter a starting number");
-            int startNum = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter a starting number");
+            //int startNum = int.Parse(Console.ReadLine());
 
-            for (int i = startNum; i >= 0; i--)
+            //for (int i = startNum; i >=0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("Liftoff!");
+            /////////////////////////////////////////
+
+            //Task-2 Sum of number 1 to N
+            Console.WriteLine("Enter a positive whole number");
+            int n = int.Parse(Console.ReadLine());
+
+            int Sum = 0;
+
+            for (int i = 1; i <= n; i++)
             {
-                Console.WriteLine(i);
+                Sum = Sum + i;
+                Console.WriteLine(Sum);
             }
-            Console.WriteLine("Liftoff!");
-
-
         }
     }
 }
