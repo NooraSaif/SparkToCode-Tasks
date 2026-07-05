a790f0eb3e120e9468476f68e4b7b7616b6c8a07
Noora AlHusaini <hlhussaini402@gmail.com>
2026-07-05 17:31:31 +0400

    implemented Task 2 - Power & Root Explorer

--- Task3Functions/Program.cs
+++ Task3Functions/Program.cs
@@ -5,15 +5,28 @@
         static void Main(string[] args)
         {
             //Task-1 Absolute Difference
-            Console.WriteLine("Enter the first number: ");
-            int num1 = Convert.ToInt32 (Console.ReadLine());
+            //Console.WriteLine("Enter the first number: ");
+            //int num1 = Convert.ToInt32 (Console.ReadLine());
 
-            Console.WriteLine("Enter the second number: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the second number: ");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
 
-            int result = num2 - num1;
-            Console.WriteLine($"The absolute difference between {num1} and {num2} is: {Math.Abs(result)}");
+            //int result = num2 - num1;
+            //Console.WriteLine($"The absolute difference between {num1} and {num2} is: {Math.Abs(result)}");
+            ////////////////////////////////////////////////////////////////////////////////////////////////
 
+            //Task 2 - Power & Root Explorer
+            Console.WriteLine("Enter a number: ");
+            double num = Convert.ToInt32(Console.ReadLine());
+
+            double result1 = Math.Pow(num, 2);
+            double result2 = Math.Sqrt(num);
+
+            Console.WriteLine($"The square of {num} is: {result1}");
+            Console.WriteLine($"The square root of {num} is: {result2}");
+            ///////////////////////////////////////////////////////////////
+
+            
         }
     }
 }
