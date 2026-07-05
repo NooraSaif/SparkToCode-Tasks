c58e485e5eeabe36e3849d4a1ebaa21a6ff47e1f
Noora AlHusaini <hlhussaini402@gmail.com>
2026-07-05 17:23:12 +0400

    implemented Task-1 Absolute Difference

--- Task3Functions/Program.cs
+++ Task3Functions/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            
+            //Task-1 Absolute Difference
+            Console.WriteLine("Enter the first number: ");
+            int num1 = Convert.ToInt32 (Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = num2 - num1;
+            Console.WriteLine($"The absolute difference between {num1} and {num2} is: {Math.Abs(result)}");
+
         }
     }
 }
