80712995a4cf76cba6085be635428e8f7170a6ee
Noora AlHusaini <hlhussaini402@gmail.com>
2026-07-05 17:45:39 +0400

    implemented Task 3 - Name Formatter

--- Task3Functions/Program.cs
+++ Task3Functions/Program.cs
@@ -16,17 +16,26 @@
             ////////////////////////////////////////////////////////////////////////////////////////////////
 
             //Task 2 - Power & Root Explorer
-            Console.WriteLine("Enter a number: ");
-            double num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter a number: ");
+            //double num = Convert.ToInt32(Console.ReadLine());
 
-            double result1 = Math.Pow(num, 2);
-            double result2 = Math.Sqrt(num);
+            //double result1 = Math.Pow(num, 2);
+            //double result2 = Math.Sqrt(num);
 
-            Console.WriteLine($"The square of {num} is: {result1}");
-            Console.WriteLine($"The square root of {num} is: {result2}");
+            //Console.WriteLine($"The square of {num} is: {result1}");
+            //Console.WriteLine($"The square root of {num} is: {result2}");
             ///////////////////////////////////////////////////////////////
 
+            //Task 3 - Name Formatter
+            Console.WriteLine("Enter your full name: ");
+            string UserName = Console.ReadLine();
+
+            Console.WriteLine("Your name in uppercase: " + UserName.ToUpper());
+            Console.WriteLine("Your name in lowercase: " + UserName.ToLower());
+            Console.WriteLine("the number of characters in your name is: " + UserName.Length);
+            //////////////////////////////////////////////////////
             
+
         }
     }
 }
